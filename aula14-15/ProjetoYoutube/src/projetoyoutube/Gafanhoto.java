9a32ca81aad04ef41ec7fd4e77f3b277f0f79dc1
Pedro <pedroras004@gmail.com>
2020-12-15 17:54:45 -0300

    AULA12-15

---
+++ aula14-15/ProjetoYoutube/src/projetoyoutube/Gafanhoto.java
@@ -0,0 +1,34 @@
+package projetoyoutube;
+
+public class Gafanhoto extends Pessoa {
+    private String login;
+    private int totAssistido;
+
+    public Gafanhoto(String nome, String sexo, int idade, String login) {
+        super(nome, sexo, idade);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Gafanhoto [" + super.toString() + "login=" + login + ", totAssistido=" + totAssistido + "]";
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
+}
